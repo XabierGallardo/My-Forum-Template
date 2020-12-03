a4f9f52b8d308e853fe80c19d4008db4605a6f79
XabierGallardo <javiergallard91@gmail.com>
2020-12-03 01:15:41 +0100

    Added browser's database localStorage

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
-// Array of thread objects
-var threads = [
+// Array of default thread objects
+var defaultThreads = [
 
 	{
 		id: 1,
@@ -45,3 +45,21 @@ var threads = [
 		]
 	}
 ]
+
+
+// Due to this front-end template, instead of a database, we'll use localStorage
+var threads = defaultThreads;
+
+//Check localStorage and see if key 'threads' is available
+if(localStorage && localStorage.getItem('threads')) {
+
+	threads = JSON.parse(localStorage.getItem('threads')); // Pull it out
+
+} else {
+
+	// Set it as value of defaultThreads
+	threads = defaultThreads;
+
+	// JSON.parse to convert to an object and JSON.stringify to convert it a string to push onto the storage
+	localStorage.setItem('threads', JSON.stringify(defaultThreads));
+}
